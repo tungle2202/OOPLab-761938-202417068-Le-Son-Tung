4c1c9eed43668276193f920563c3baf5bb1b359c
Le Son Tung <tung.ls110106@gmail.com>
2026-03-27 15:47:51 +0700

    Complete assignment 2.2.1

--- week1/HelloWorld.java
+++ week1/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello World!");
-		System.out.print("Xin chao cac \n ban");
+		System.out.println("Xin chao \n cac ban!");
+		System.out.println("Hello \t world!");
 	}
 }
