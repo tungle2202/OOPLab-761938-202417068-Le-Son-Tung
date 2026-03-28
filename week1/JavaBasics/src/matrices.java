d7a643950271f6168cfe20a95f34001ce76db550
Le Son Tung <tung.ls110106@gmail.com>
2026-03-28 22:00:46 +0700

    6.6

---
+++ week1/JavaBasics/src/matrices.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class matrices {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrixA = new int[rows][cols];
+        int[][] matrixB = new int[rows][cols];
+        int[][] sumMatrix = new int[rows][cols];
+
+        System.out.println("Enter elements for Matrix A:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.printf("A[%d][%d] = ", i, j);
+                matrixA[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter elements for Matrix B:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.printf("B[%d][%d] = ", i, j);
+                matrixB[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sumMatrix[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+
+        System.out.println("\nResulting Matrix (A + B):");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sumMatrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
