4b4ffde2ef8a1382ed6be73dcad6e35fe9506d1a
Le Son Tung <tung.ls110106@gmail.com>
2026-04-05 21:45:32 +0700

    this is a msg

---
+++ lab02/AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,27 @@
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+}
