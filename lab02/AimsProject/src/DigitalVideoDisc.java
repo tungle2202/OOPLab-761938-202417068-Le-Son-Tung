07cf339f98ac07f7cc608537ff9a9fd381b7b7c4
Le Son Tung <tung.ls110106@gmail.com>
2026-04-05 21:54:05 +0700

    this is another msg

--- lab02/AimsProject/src/DigitalVideoDisc.java
+++ lab02/AimsProject/src/DigitalVideoDisc.java
@@ -24,4 +24,30 @@ public class DigitalVideoDisc {
     public float getCost() {
         return cost;
     }
+
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
 }
