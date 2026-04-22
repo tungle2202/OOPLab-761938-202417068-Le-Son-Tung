36f741a3451652566aded13ce98fc1fb3cd40aaa
Le Son Tung <tung.ls110106@gmail.com>
2026-04-23 00:11:07 +0700

    done to string / de bai khong yeu cau thiet ke lai method toString()

--- AimsProject/src/hust/soict/hedspi/test/cart/cartTest.java
+++ AimsProject/src/hust/soict/hedspi/test/cart/cartTest.java
@@ -1,18 +1,22 @@
-//package hust.soict.hedspi.test.cart;
-//
-//import hust.soict.hedspi.aims.cart.Cart;
-//import hust.soict.hedspi.aims.media.DigitalVideoDisc;
-//
-//public class cartTest{
-//    public static void main(String[] args) {
-//        Cart cart = new Cart();
-////        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-////        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Scifi", "George Lucas", 87, 24.95f);
-////        cart.addDigitalVideoDisc(dvd1, dvd2);
-////        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-////        cart.addDigitalVideoDisc((dvd3));
-//
-////        cart.search("The Lion King");
-////        cart.search(20);
-////    }
-//}
+package hust.soict.hedspi.test.cart;
+
+import hust.soict.hedspi.aims.cart.Cart;
+import hust.soict.hedspi.aims.media.Book;
+import hust.soict.hedspi.aims.media.CompactDisc;
+import hust.soict.hedspi.aims.media.DigitalVideoDisc;
+import hust.soict.hedspi.aims.media.Media;
+
+import java.util.ArrayList;
+
+public class cartTest{
+    public static void main(String[] args) {
+        ArrayList<Media> hihi = new ArrayList<Media>();
+        Book hehe = new Book(1, "rung na uy", "tieu thuyet", 100);
+        CompactDisc huhu = new CompactDisc(1, "Cai thu 2", "Alt rock", 350, 45, "Thang Ngot", "Thang ngot");
+        hihi.add(hehe);
+        hihi.add(huhu);
+        for(Media m: hihi){
+            System.out.println(m.toString());
+        }
+    }
+}
