dea24db8ccd7b78d2e7a0cd9980438d4dbee864f
Le Son Tung <tung.ls110106@gmail.com>
2026-04-22 23:09:54 +0700

    store updated

--- AimsProject/src/hust/soict/hedspi/aims/store/Store.java
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -1,43 +1,26 @@
 package hust.soict.hedspi.aims.store;
 import hust.soict.hedspi.aims.media.DigitalVideoDisc;
+import hust.soict.hedspi.aims.media.Media;
+import java.util.ArrayList;
 
 public class Store {
-    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[100];
-    private int id = 0;
-    public void addDVD(DigitalVideoDisc a){
-        boolean existed = false;
-        if(this.id != 0) {
-            for (DigitalVideoDisc b : itemsInStore) {
-                if (a == b) {
-                    existed = true;
-                    break;
-                }
-            }
-        }
-        if(existed){
-            System.out.println("DVD " + a.getTitle() + " Da ton tai!");
+    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
+    //private int id = 0;
+    public void addDVD(Media a){
+        if(itemsInStore.contains(a)){
+            System.out.println("San pham da ton tai!");
         }else {
-            itemsInStore[id] = a;
-            id++;
-            System.out.println("DVD " + a.getTitle() + "Da duoc them thanh cong!");
+            itemsInStore.add(a);
+            System.out.println("San pham da duoc them thanh cong!");
         }
     }
 
     public void removeDVD(DigitalVideoDisc a){
-        boolean isRemoved = false;
-        for(int i = 0; i<=id; i++){
-            if(itemsInStore[i]==a){
-                for(int j = i; j<id; j++){
-                    itemsInStore[j] = itemsInStore[j+1];
-                }
-                id--;
-                isRemoved = true;
-            }
-        }
-        if(isRemoved){
-           System.out.println("Da xoa thanh cong DVD: " + a.getTitle());
+        if(itemsInStore.contains(a)){
+            itemsInStore.remove(a);
+            System.out.println("Da xoa thanh cong san pham");
         }else{
-            System.out.println("Khong tim thay DVD: " + a.getTitle());
+            System.out.println("Khong ton tai san pham!");
         }
     }
 
