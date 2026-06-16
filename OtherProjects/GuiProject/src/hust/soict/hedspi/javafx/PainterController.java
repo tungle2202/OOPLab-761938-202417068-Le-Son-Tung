45f5b28ba3e036214188d7d40aa660406bb99760
Le Son Tung <tung.ls110106@gmail.com>
2026-06-16 19:38:20 +0700

    feat: eraser tool added

--- OtherProjects/GuiProject/src/hust/soict/hedspi/javafx/PainterController.java
+++ OtherProjects/GuiProject/src/hust/soict/hedspi/javafx/PainterController.java
@@ -14,10 +14,21 @@ public class PainterController {
     void clearButtonPressed(ActionEvent event) {
         drawingAreaPane.getChildren().clear();
     }
+
+    private Color brushColor = Color.BLACK;
+
+
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-        Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
+        Circle newCircle = new Circle(event.getX(), event.getY(), 4, brushColor);
         drawingAreaPane.getChildren().add(newCircle);
     }
 
+    public void penButtonClicked(MouseEvent mouseEvent) {
+        brushColor = Color.BLACK;
+    }
+
+    public void eraserButtonClicked(MouseEvent mouseEvent) {
+        brushColor = Color.WHITE;
+    }
 }
